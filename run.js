a3b78ebfe7daf63c5c5b93d53306abd734375c72
root <root@mixedfruitjuice.ovh.net>
2017-07-24 13:10:52 +0200

    Added a function to play Yodelmusic

--- run.js
+++ run.js
@@ -10,6 +10,7 @@ var messages = {
 	"waarom besta ik?" : "omdat het kan jij uit de baarmoeder geslingerde aardappelzetmeeloplossingswattenstaafje"
 };
 
+var muziek = ['jodelmuziek.mp3', 'jodelmuziek2.mp3'];
 var mentionReplies = ["Donder op joe met je graftakkenface", "Beter begin je je kop te houden anders maak ik je kop nog lelijker dan die nu al is", "Laat me met rust, bots hebben ook een leven", "Nee flikker op"];
 
 client.on('ready', () => {
@@ -17,24 +18,49 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-    var content = msg.content.toLowerCase();
-    var number = Math.floor(Math.random() * 4);
+	var content = msg.content.toLowerCase();
+	var number = Math.floor(Math.random() * 4);
+	var muzieknumber = Math.floor(Math.random() * 2);
 
-    if (messages[content]) {
-	msg.reply(messages[content]);
-    }
+	if (messages[content]) {
+		msg.reply(messages[content]);
+	}
 
-    if (msg.isMentioned(client.user)) {
-        msg.reply(mentionReplies[number]);
-    }
+	if (msg.isMentioned(client.user)) {
+	    msg.reply(mentionReplies[number]);
+	}
 
-   if (msg.content.startsWith("spam") && msg.content.length > 5) {
-	while (number < 30) {	
-		msg.channel.sendMessage(msg.content.substr(4, 60));
-  		number++
- 	}
-   }
+	if (msg.content.startsWith("spam") && msg.content.length > 5) {
+		while (number < 30) {	
+			msg.channel.sendMessage(msg.content.substr(4, 60));
+			number++;
+		}
+	}
+
+	if (msg.content === '/jodelmuziek') {
+		if (msg.member.voiceChannel) {
+				msg.member.voiceChannel.join()
+			.then(connection => { 
+		 		msg.reply('Bereidt je oren maar alvast voor');
+		 		const dispatcher = connection.playFile(muziek[muzieknumber]);
+			})
+			.catch(console.log);
+		} else {
+			msg.reply('Je moet eerst in een voicechannel zitten idioot');
+		}
+	}
+
+	if (msg.content === '/mijnorendoenpijn') {
+		if (msg.member.voiceChannel) {
+				msg.member.voiceChannel.leave();
+		 		msg.reply('Ik had je gewaarschuwd');
+		} else {
+			msg.reply('Ik zit niet in een voicechannel stomme hond');
+		}
+	}
 });
+
+
 
 client.on('guildMemberAdd', member => {
     member.sendMessage("Welcome bij de Jodel Vereniging! PM Justen of Jeroen als je hulp nodig hebt! :wink: Neem ook even een kijkje bij #welcome en #karma. Veel plezier!");
